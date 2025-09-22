29315a816ee8e5311c8814ba81aff24e6d3559fc
voltaan <voltaan@proton.me>
2025-09-22 19:27:16 +0200

    Aggiunta documentazione in MinimoComune.java

--- src/MinimoComune.java
+++ src/MinimoComune.java
@@ -1,8 +1,22 @@
 public class MinimoComune {
+     /**
+     * Calcola il minimo comune multiplo di 2 numeri.
+     *
+     * @param x Numero 1.
+     * @param y Numero 2.
+     * @return Il minimo comune multiplo dei numeri dati.
+     */
     public static int mcm(int x, int y) {
         return (x * y)/mcd(x,y);
     }
 
+    /**
+     * Calcola il massimo comune divisore di 2 numeri.
+     *
+     * @param x Numero 1.
+     * @param y Numero 2.
+     * @return Il massimo comune divisore dei numeri dati.
+     */
     public static int mcd(int x, int y) {
         int resto;
         while (y != 0) {
