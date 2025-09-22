fe496a77a2f5a4c912af9a5ada51b0cf517f13fe
voltaan <voltaan@proton.me>
2025-09-22 19:30:53 +0200

    Aggiunta documentazione in App.java

--- src/App.java
+++ src/App.java
@@ -1,4 +1,8 @@
 public class App {
+     /**
+     * Punto di partenza del programma. 
+     * @param args Argomenti di linea di comando.
+     */
     public static void main(String[] args) throws Exception {
         Utenti login = new Utenti();
         login.Autenticazione();
